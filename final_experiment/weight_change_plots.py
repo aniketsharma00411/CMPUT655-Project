c9d4163586c71121b8dd9f1216f014399f1fa48c
Aniket Sharma <aniketsharma00411@gmail.com>
2023-12-14 02:11:58 -0700

    Changes in weight script

--- final_experiment/weight_change_plots.py
+++ final_experiment/weight_change_plots.py
@@ -1,8 +1,11 @@
+# python final_experiment/weight_change_plots.py ./final_experiment <model-name> <run>
 
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import sys
+
 
 def return_weights(weights):
     core = []
@@ -55,26 +58,28 @@ def plot_weight_change(env_l2_norm_dict_list, env):
                 preprocessor_l2_norms.append(l2_norm[key])
             if key == "together":
                 total_l2_norms.append(l2_norm[key])
-    os.makedirs(f"plots_for_weight_change", exist_ok=True)
+
     # plotting the total weight change l2 norm for the environment over all cycles and stages of training
     lst_of_wight_types = ["total ", "core", "actor", "critic", "preprocessor"]
 
-    for index, x in enumerate([total_l2_norms, core_l2_norms, actor_l2_norms, critic_l2_norms, preprocessor_l2_norms]):
+    for _, x in enumerate([total_l2_norms, core_l2_norms, actor_l2_norms, critic_l2_norms, preprocessor_l2_norms]):
 
         # normalize the l2_norms with the first l2_norm in x to be the 1
         x = np.array(x)
         x = x / x[0]
 
-
-        plt.plot(range(1, len(x) + 1), x, marker="o", markersize=3, linewidth=1)
+        plt.plot(range(1, len(x) + 1), x, marker="o",
+                 markersize=3, linewidth=1)
         plt.xlabel("Visit Number")
         plt.ylabel("Weights")
 
         plt.xticks(range(1, len(x) + 1))
-        #add a legend where the first argument is the label and the second is the color
-    plt.title(f"Weight Change L2 Norm for {env}")
+        # add a legend where the first argument is the label and the second is the color
+    plt.title(f"Weight Change L2 Norm for {env} - {model_name}")
+    plt.ylim((0, 1.5))
     plt.legend(lst_of_wight_types, loc="upper right")
-    plt.savefig(f"plots_for_weight_change/weight_change_l2_norm_{env}.png")
+    plt.savefig(os.path.join(
+        path, model_name, run, "weight_change_graphs", f"weight_change_l2_norm_{env}.png"))
     plt.close()
 
 
@@ -86,43 +91,58 @@ def init_weights(path):
         return {"core": core, "actor": actor, "critic": critic, "preprocessor": preprocessor, "together": together}
 
 
-def main(cycle_count, weights_folder_pth, envs_list):
-    for env in envs_list:
+def main(num_of_cycles, weights_folder_pth, envs):
+    for env in envs:
         env_l2_norm_dict_list = []
-
 
         for file in sorted(os.listdir(weights_folder_pth)):
             meta_data = file.split("_")
             env_name = meta_data[4]
 
             if env_name == env:
-                for cycle in range(0, cycle_count):
+                for cycle in range(0, num_of_cycles):
                     prev_weights_dict = dict()
                     init_flag = True
                     for stage in [1, 2]:
-                        pth = weights_folder_pth + "/" + f"weights_cycle_{cycle}_env_{env_name}_stage_{stage}.pkl"
+                        pth = weights_folder_pth + "/" + \
+                            f"weights_cycle_{cycle}_env_{env_name}_stage_{stage}.pkl"
                         with open(pth, "rb") as pth:
                             weights = pickle.load(pth)
-                            print("for cycle:", cycle, "stage:", stage, "env:", env_name, ", weights loaded")
-                            core, actor, critic, preprocessor, together = return_weights(weights)
+                            print("for cycle:", cycle, "stage:", stage,
+                                  "env:", env_name, ", weights loaded")
+                            core, actor, critic, preprocessor, together = return_weights(
+                                weights)
                             curr_weight_dict = {"core": core, "actor": actor, "critic": critic,
                                                 "preprocessor": preprocessor, "together": together}
                             if init_flag == False:
-                                weight_change_l2_norms = calc_l2_norm(curr_weight_dict, prev_weights_dict)
+                                weight_change_l2_norms = calc_l2_norm(
+                                    curr_weight_dict, prev_weights_dict)
                                 for key in weight_change_l2_norms.keys():
-                                    print(key, ":", weight_change_l2_norms[key])
+                                    print(
+                                        key, ":", weight_change_l2_norms[key])
                                 print('curr_weights', curr_weight_dict)
                                 print("prev weights", prev_weights_dict)
-                                env_l2_norm_dict_list.append(weight_change_l2_norms)
+                                env_l2_norm_dict_list.append(
+                                    weight_change_l2_norms)
                         prev_weights_dict = curr_weight_dict
 
                         if init_flag == True:
                             init_flag = False
                 break
+
         plot_weight_change(env_l2_norm_dict_list, env)
+
+
 if __name__ == '__main__':
-    main(cycle_count=3,
-         weights_folder_pth="/Users/srinjoybhuiya/PycharmProjects/CMPUT655-Project/final_experiment/gru/run2/saved_weights",
-         envs_list=["LabyrinthEscapeAlmostEasy", "LabyrinthEscapeEasy", "LabyrinthEscapeVeryEasy"])
+    path = sys.argv[1]
+    model_name = sys.argv[2]
+    run = sys.argv[3]
 
-
+    envs = ["LabyrinthExploreEasy", "LabyrinthEscapeMedium", "LabyrinthExploreAlmostEasy",
+            "LabyrinthEscapeEasy", "LabyrinthExploreMedium", "LabyrinthEscapeAlmostEasy"]
+    main(
+        num_of_cycles=10,
+        weights_folder_pth=os.path.join(
+            path, model_name, run, "saved_weights"),
+        envs=envs
+    )
